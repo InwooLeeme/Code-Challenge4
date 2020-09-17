698b3d80a893844901dd178b0d96b380ec329675
ItsmeLee <arsmagica90@gmail.com>
2020-09-17 13:42:42 +0900

    Complete Code Challenge
    Add two middlewares called console and protected
    
    Console middleware outputs I'm middleware in the console window when the user connects to each path.
    The protected middleware won't allow me to go to "/protected", if user try to go to "/protected" the user should be redirected back to "/".

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 4000;
 
 const handleListening = () => {
-  console.log(`Listening!`);
+  console.log("Listening");
 };
 
 const handleHome = (req, res) => {
@@ -23,11 +23,24 @@ const handleProtected = (req, res) => {
   res.send("Protected");
 };
 
+// Middleware zone
+
+const Console = (req, res, next) => {
+  console.log(`I'm middlewares`);
+  next();
+};
+
+const Protected = (req, res, next) => {
+  console.log(`This page is protected`);
+  res.redirect("/");
+};
+
 // routes
+app.use(Console);
 app.get("/", handleHome);
 app.get("/about-us", handleAboutUs);
 app.get("/contact", handleContact);
-app.get("/protected", handleProtected);
+app.get("/protected", Protected, handleProtected);
 
 // Codesanbox does not need PORT :)
 app.listen(PORT, handleListening);
